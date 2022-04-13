cc8c3664c00053a83c7ee52f1ab902927b0a9854
gyojincompany <luckyred@gmail.com>
2022-04-13 16:59:54 +0900

    외부설정java 불러오기

---
+++ src/main/java/com/javagyojin/ex/ApplicationConfig_dev.java
@@ -0,0 +1,23 @@
+package com.javagyojin.ex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ApplicationConfig_dev {
+	
+	@Bean
+	public ServerInfo serverInfo() {
+		
+		ServerInfo sInfo = new ServerInfo();
+		
+		sInfo.setIpNum("localhost");
+		sInfo.setPortNum("8888");
+		
+		return sInfo;
+	}
+	
+	
+}
---
+++ src/main/java/com/javagyojin/ex/ApplicationConfig_run.java
@@ -0,0 +1,22 @@
+package com.javagyojin.ex;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("run")
+public class ApplicationConfig_run {
+	
+	@Bean
+	public ServerInfo serverInfo() {
+		
+		ServerInfo sInfo = new ServerInfo();
+		
+		sInfo.setIpNum("211.5.20.121");
+		sInfo.setPortNum("80");
+		
+		return sInfo;
+	}
+}
